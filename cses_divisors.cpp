2ffb81e57cf9d6d393fe60b126924a52b72c0098
akkapakakarthik <155606355+akkapakakarthik@users.noreply.github.com>
2024-07-10 11:56:09 +0530

    Add files via upload

--- cses_divisors.cpp
+++ cses_divisors.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 int main () {
-    int t, a;
+    int t, a, ans;
     cin >> t;
     for (int i = 0; i < t; i++){
         cin >> a;
         int divisors = 0;
-        for (int j = 1; j <= a; j++){
+        for (int j = 1; j*j <= a; j++){
             if(a%j == 0) divisors ++;
         }
+        if (divisors % 2 == 0) 
         cout << divisors << endl;
     }
 }
